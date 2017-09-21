b2e0f4905005903dd9e8c3a13efb16791682bcab
Kollol <kollol303@gmail.com>
2017-09-21 15:07:15 +0600

    Add files via upload

---
+++ AppKiller/Class/Killer.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System;
+
+namespace AppKiller
+{
+    class Killer
+    {
+        public void kill()
+        {            
+            if (File.Exists(@"AppsToKill.txt"))
+            {
+                using (StreamReader sr = new StreamReader(@"AppsToKill.txt"))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        var line = sr.ReadLine();
+                        if (line.Length > 0)
+                        {
+                            try
+                            {
+                                Process[] process = Process.GetProcessesByName(line);
+                                foreach (var item in process)
+                                {
+                                    try
+                                    {
+                                        item.Kill();
+                                    }
+                                    catch (Exception)
+                                    {
+                                        Console.WriteLine("Process {0} could not be killed successfully.", item);
+                                    }                                    
+                                }
+                            }
+                            catch (System.Exception)
+                            {
+                                Console.WriteLine("Oops! Something went wrong while killing a process!");
+                            }                            
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("The file 'AppsToKill.txt' does not exist in application location. Nothing to kill!");
+            }            
+        }
+    }
+}
